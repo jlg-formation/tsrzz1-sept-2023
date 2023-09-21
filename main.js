08292f759975aeb870baa2fd2b4021e2f0ab2d1e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-09-21 13:25:40 +0200

    dev: added circles on big circle

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const samples = 10;
 const svgns = "http://www.w3.org/2000/svg";
 const container = document.querySelector("g.samples");
-console.log("container: ", container);
+
+const cx0 = 0;
+const cy0 = 0;
+const r0 = 55;
 
 for (let i = 0; i < samples; i++) {
   const circle = document.createElementNS(svgns, "circle");
-  circle.setAttributeNS(null, "cx", 34);
-  circle.setAttributeNS(null, "cy", 23);
+
+  const angle = i * ((2 * Math.PI) / samples) + Math.PI / 2;
+
+  const cx = cx0 + r0 * Math.cos(angle);
+  const cy = cy0 + r0 * Math.sin(angle);
+
+  circle.setAttributeNS(null, "cx", cx);
+  circle.setAttributeNS(null, "cy", cy);
   circle.setAttributeNS(null, "r", 1);
   container.appendChild(circle);
 }
